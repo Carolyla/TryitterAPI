e19d9620b99d07e814fdae3179d10a9a780d29ed
Carol Silva <carolyla@hotmail.com>
2022-12-15 17:14:58 -0300

    feat: criação da model Users e Studant

---
+++ Models/Posts.cs
@@ -0,0 +1,11 @@
+namespace TryitterApi.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
---
+++ Models/Studant.cs
@@ -0,0 +1,14 @@
+namespace TryitterApi.Models
+{
+    public class Studant
+    {
+        public int StudandId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+    }
+    
+}
