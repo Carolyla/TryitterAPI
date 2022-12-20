8f42c27af37c5389569516c1b7e5adf4d4cdb320
Carol Silva <carolyla@hotmail.com>
2022-12-20 16:40:43 -0300

    feat: populando DB, tabela Students

---
+++ Migrations/20221220181648_PopulaStudents.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TryitterAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaStudents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb) 
+        {
+            mb.Sql("Insert into Students(Name, Email, Password) Values('Isabela Pontes', 'isabelapontes@email.com', 'senha123')");
+            mb.Sql("Insert into Students(Name, Email, Password) Values('Thiago Silva', 'tiagosilva@email.com', '12345678')");
+            mb.Sql("Insert into Students(Name, Email, Password) Values('Maria Clara', 'clarinha@email.com', 'abcdefg')");
+        }
+     
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Students");
+        }
+    }
+}
