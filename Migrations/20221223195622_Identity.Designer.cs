46d0689ff0d92efd9d3d960e543b3b4dbdf8d431
Carol Silva <carolyla@hotmail.com>
2022-12-27 11:54:31 -0300

    feat: instalando o framework identity

--- Migrations/20221223195622_Identity.Designer.cs
+++ Migrations/20221223195622_Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TryitterApi.Context;
 
@@ -11,9 +12,11 @@ using TryitterApi.Context;
 namespace TryitterAPI.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20221223195622_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
