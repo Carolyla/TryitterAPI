4b0fef0f422782a8ac7012f8658eb054e18b9236
Carol Silva <carolyla@hotmail.com>
2022-12-29 15:18:45 -0300

    feat: Implementada a geração do token

--- Controllers/AutorizaController.cs
+++ Controllers/AutorizaController.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TryitterApi.DTOs;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace TryitterApi.Controllers
 {
@@ -10,6 +15,7 @@ namespace TryitterApi.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
 
         public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
@@ -43,7 +49,7 @@ namespace TryitterApi.Controllers
                 return BadRequest(result.Errors);
             }
             await _signInManager.SignInAsync(user, false);
-            return Ok();
+            return Ok(GeraToken(model));
 
         }
         [HttpPost("Login")]
@@ -59,7 +65,7 @@ namespace TryitterApi.Controllers
             
             if (result.Succeeded)
             {
-                return Ok();
+                return Ok(GeraToken(userInfo));
             }
 
             else
@@ -68,5 +74,43 @@ namespace TryitterApi.Controllers
                 return BadRequest(ModelState);
             }
         }
+        private UserToken GeraToken(UserDTO userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, userInfo.Email),
+                new Claim("meuClain", "cripto"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            // gera uma chave com base em um algoritimo simetrico
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+           
+            // gera uma ass digital do token usando a chave privada e o algoritimo Hmac.
+            var credenciais = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            
+            //configura o tempo de expiração do toquem com base na chave no appsettings
+            var expConfig = _configuration["TokenConfiguration:ExpireHours"];
+            var expiration = DateTime.UtcNow.AddHours(double.Parse(expConfig));
+
+            //classe que representa um token JWT e gera token
+            JwtSecurityToken token = new(
+                issuer: _configuration["TokenConfiguration:Issuer"],
+                audience: _configuration["TokenConfiguration:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credenciais);
+
+            return new UserToken()
+            {
+                Authenticated = true,
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration,
+                Message = "Token JWT OK"
+            };   
+            
+
+        }
+
+
     }
 }
